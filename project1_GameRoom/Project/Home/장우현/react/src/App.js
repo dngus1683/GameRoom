c7b352568c7d820b6022189a776b8998b60587c9
dngus1683 <71319694+dngus1683@users.noreply.github.com>
2022-06-02 00:00:55 +0900

    7주차 home화면 react로 수정1

---
+++ project1_GameRoom/Project/Home/장우현/react/src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import React, {useState} from 'react'
+
+function LoginRow(){
+  return(
+    <div class="login_row">
+      <div class="login">
+        <a href="Login.html"><img src={require('./Img/icon_Login.png')}/></a>
+      </div>
+      <div class="login">
+        <a href="Login.html" class="login_a">로그인</a>
+      </div>
+    </div>
+  );
+}
+
+function Head(){
+  return(
+    <div>
+      <h1 class="Home_head">GameRoom</h1>
+    </div>
+  );
+}
+
+function MenuRow(props){
+  var listTag = [];
+  for(var i=0;i<props.list.length;i++){
+    var li = props.list[i];
+    listTag.push(
+        <div class="menu"><a>{li.title}</a></div>
+    )
+  }
+  return(
+    <div class="menu_row">
+      {listTag}
+    </div>
+  );
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <LoginRow/>
+      <Head/>
+      <MenuRow list={list.json}/>
+    </div>
+  );
+}
+
+export default App;
